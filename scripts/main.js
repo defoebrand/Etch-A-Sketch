90dc46b1073b5180af2dbadc4eb8473e38ee3720
Brandon Defoe <defoe.brand@gmail.com>
2020-04-05 02:26:14 +0300

    First commit of working project

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+const root = document.documentElement;
+
+const container = document.querySelector("#container");
+container.classList.add("container");
+
+const gridBox = document.querySelector("#gridBox");
+gridBox.classList.add("grid");
+container.appendChild(gridBox);
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", function(e){location.reload()});
+
+const clean = document.querySelector("#clean");
+gridBox.appendChild(clean);
+
+let rows = prompt("How many rows?");
+let columns = prompt("How many columns?");
+
+let num = rows * columns;
+
+root.style.setProperty("--gridRows", rows);
+root.style.setProperty("--gridColumns", columns);
+
+for(let i = 0; i <= num; i++) {
+    let div = document.createElement("div");
+    div.classList.add("gridSquares");
+	div.addEventListener("mouseover", () => {
+	div.classList.add("hover");
+	});
+	div.addEventListener("dragover", () => {
+	div.classList.remove("hover");
+	});
+    gridBox.appendChild(div);
+}
+
+/*
+
+
+
+*/
+
+
+
+
+
+
